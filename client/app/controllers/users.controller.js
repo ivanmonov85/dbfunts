832789f0a3b13fb808ac795cb6655f48a7b7bbda
ivanmonov85 <ivanmonov85@gmail.com>
2022-03-01 10:26:09 +0200

    fixes and cleanup

--- client/app/controllers/users.controller.js
+++ client/app/controllers/users.controller.js
@@ -5,31 +5,43 @@ angular.module('users-controller', [
     'ngResource', 
     'ngDialog'
 ])
-.controller('UsersController', function($scope, NgTableParams, $resource, $localStorage, ngDialog, env) {
-
-	$scope.user = $resource(env.BASE_URL + '/users/:id', { id: '@id' });
+.controller('UsersController', function($scope, NgTableParams, $resource, ngDialog, env) {
+    $scope.user = {};
 
 	$scope.users = $resource(env.BASE_URL + '/users/:id', { id: '@id' }, {
-        'save': { method: 'POST' },
-		'update': { method: 'PUT' }
+        // default actions - can be changed
+        //'get': { method: 'GET' }
+        //'query': { method: 'GET', isArray: true },
+        //'save': { method: 'POST' },
+        //'delete': { method: 'DELETE' },
+
+        // added custom actions
+        'update': { method: 'PUT' }
+        //'patch': { method: 'PATCH' }
 	});
 
     $scope.tableParams = new NgTableParams({}, {
-		//GET /api/users?page={page}&size={size}&sortDir={dir}&sort={propertyName}
-        getData: function(params) {
-            var queryParams = {
-                page: params.page() - 1, 
-                size: params.count()
-            };
-            var sortingProp = Object.keys(params.sorting());
-            if (sortingProp.length == 1) {
-                queryParams['sort'] = sortingProp[0];
-                queryParams['sortDir'] = params.sorting()[sortingProp[0]];
-            }
-			queryParams = {}; // no pagination is supported yet
-            return $scope.users.query(queryParams, function(data, headers) {
-                //var totalRecords = headers('PAGING_INFO').split(',')[0].split('=')[1];
-                //params.total(totalRecords);
+		// GET /api/users?page={page}&size={size}&sortDir={dir}&sort={propertyName} - with pagination
+        // getData: function(params) {
+        //     var queryParams = {
+        //         page: params.page() - 1, 
+        //         size: params.count()
+        //     };
+        //     var sortingProp = Object.keys(params.sorting());
+        //     if (sortingProp.length == 1) {
+        //         queryParams['sort'] = sortingProp[0];
+        //         queryParams['sortDir'] = params.sorting()[sortingProp[0]];
+        //     }
+        //     return $scope.users.query(queryParams, function(data, headers) {
+        //         var totalRecords = headers('PAGING_INFO').split(',')[0].split('=')[1];
+        //         params.total(totalRecords);
+        //         return data;
+        //     }).$promise;
+        // }
+
+        // GET /api/users - without pagination
+        getData: function() {
+            return $scope.users.query({}, function(data) {
                 return data;
             }).$promise;
         }
@@ -37,12 +49,12 @@ angular.module('users-controller', [
 
 	$scope.addNewUser = function() {
         $scope.user = { name: 'John Doe', username: '', email: '', phone: '', website: '' };
-        ngDialog.open({ template: 'components/user.dialog.html', scope: $scope });
+        $scope.openModal();
     }
 
 	$scope.editUser = function(row) {
-        $scope.user = row;
-        ngDialog.open({ template: 'components/user.dialog.html', scope: $scope });
+        $scope.user = $scope.users.get({ id: row.id });
+        $scope.openModal();
     }
 
 	$scope.save = function() {
@@ -80,4 +92,18 @@ angular.module('users-controller', [
             $scope.tableParams.reload();
         });
     }
+
+    $scope.openModal = function () {
+        $scope.dialog = ngDialog.open({
+            template: 'components/user.dialog.html',
+            scope: $scope
+
+            // preCloseCallback: function(value) {
+            //     if (confirm('Are you sure you want to close without saving your changes?')) {
+            //         return true;
+            //     }
+            //     return false;
+            // }
+        });
+    };
 });
